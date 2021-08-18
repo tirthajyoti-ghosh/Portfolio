a33e4334b1e812aba3fdcce7e113fbd278a8ee06
Tirthajyoti Ghosh <itirthahere@gmail.com>
2021-08-18 11:05:30 +0530

    Add portfolio section

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Hero from './components/Hero';
 import Navbar from './components/Navbar';
+import Portfolio from './components/Portfolio';
 import './styles/App.scss';
 
 const App = () => (
@@ -7,6 +8,7 @@ const App = () => (
         <Navbar />
         <main>
             <Hero />
+            <Portfolio />
         </main>
     </>
 );
