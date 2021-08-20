65d323e65d66d354ffbefb3e7d8bf48182b71f74
Tirthajyoti Ghosh <itirthahere@gmail.com>
2021-08-20 15:44:52 +0530

    Add contact section

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import About from './components/About';
+import Contact from './components/Contact';
 import Hero from './components/Hero';
 import Navbar from './components/Navbar';
 import Portfolio from './components/Portfolio';
@@ -11,6 +12,7 @@ const App = () => (
             <Hero />
             <Portfolio />
             <About />
+            <Contact />
         </main>
     </>
 );
---
+++ src/components/Contact.jsx
@@ -0,0 +1,72 @@
+/* eslint-disable max-len */
+import {
+    Form, Input, Button,
+} from 'antd';
+
+const Contact = () => {
+    const [form] = Form.useForm();
+    const { TextArea } = Input;
+
+    const submitContactForm = (values) => {
+        console.log(values);
+    };
+
+    return (
+        <section className="contact" id="contact">
+            <div className="content">
+                <h1>Contact me</h1>
+                <p>If you have an application you are interested in developing, a feature that you need built or a project that needs coding. I’d love to help with it </p>
+
+                <Form form={form} onFinish={submitContactForm}>
+                    <Form.Item
+                        name="name"
+                        rules={[
+                            {
+                                required: true,
+                                message: 'Please input your name!',
+                            },
+                        ]}
+                    >
+                        <Input placeholder="Full name" />
+                    </Form.Item>
+
+                    <Form.Item
+                        name="email"
+                        rules={[
+                            {
+                                required: true,
+                                message: 'Please input your email!',
+                            },
+                            {
+                                type: 'email',
+                                message: 'Invalid email!',
+                            },
+                        ]}
+                    >
+                        <Input placeholder="Email" />
+                    </Form.Item>
+
+                    <Form.Item
+                        name="message"
+                        rules={[
+                            {
+                                required: true,
+                                message: 'Please input your message!',
+                            },
+                        ]}
+                    >
+                        <TextArea placeholder="Write your message here" rows={7} />
+                    </Form.Item>
+
+                    <Form.Item>
+                        <Button type="primary" htmlType="submit">
+                            Get in touch
+                        </Button>
+                    </Form.Item>
+                </Form>
+            </div>
+        </section>
+    );
+};
+
+export default Contact;
