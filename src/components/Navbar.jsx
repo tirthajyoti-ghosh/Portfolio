f988edbfe5309a6a725c4fea62aa0c72dc026b5a
Tirthajyoti Ghosh <itirthahere@gmail.com>
2021-08-22 23:04:43 +0530

    Add box-shadow to navbar when scrolled down

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,31 @@
-const Navbar = () => (
-    <header>
-        <nav className="navbar">
-            <a href="/" className="logo">T. Ghosh</a>
+import React, { useState, useEffect } from 'react';
 
-            <ul className="links">
-                <li><a href="#portfolio">Portfolio</a></li>
-                <li><a href="#about">About</a></li>
-                <li><a href="#contact">Contact</a></li>
-            </ul>
-        </nav>
-    </header>
-);
+const Navbar = () => {
+    const [showBoxShadow, setShowBoxShadow] = useState(false);
+
+    useEffect(() => {
+        document.addEventListener('scroll', () => {
+            if (window.scrollY >= 130 && !showBoxShadow) {
+                setShowBoxShadow(true);
+            } else if (window.scrollY < 130 && showBoxShadow) {
+                setShowBoxShadow(false);
+            }
+        });
+    });
+
+    return (
+        <header className={showBoxShadow ? 'box-shadow' : ''}>
+            <nav className="navbar">
+                <a href="/" className="logo">T. Ghosh</a>
+
+                <ul className="links">
+                    <li><a href="#portfolio">Portfolio</a></li>
+                    <li><a href="#about">About</a></li>
+                    <li><a href="#contact">Contact</a></li>
+                </ul>
+            </nav>
+        </header>
+    );
+};
 
 export default Navbar;
