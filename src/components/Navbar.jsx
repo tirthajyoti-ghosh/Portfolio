150cc73c0e643bbab0cc082cc9e53e8a2fcb341f
Tirthajyoti Ghosh <itirthahere@gmail.com>
2021-08-24 14:32:38 +0530

    Add mobile menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,12 @@
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 import React, { useState, useEffect } from 'react';
+import { Drawer } from 'antd';
+import { CloseIcon, MenuBurgerIcon } from './Icons';
 
 const Navbar = () => {
     const [showBoxShadow, setShowBoxShadow] = useState(false);
+    const [drawerVisible, setDrawerVisible] = useState(false);
 
     useEffect(() => {
         document.addEventListener('scroll', () => {
@@ -18,12 +23,32 @@ const Navbar = () => {
             <nav className="navbar">
                 <a href="/" className="logo">T. Ghosh</a>
 
+                <button className="menu-burger" type="button" onClick={() => setDrawerVisible(true)}>
+                    <MenuBurgerIcon />
+                </button>
+
                 <ul className="links">
                     <li><a href="#portfolio">Portfolio</a></li>
                     <li><a href="#about">About</a></li>
                     <li><a href="#contact">Contact</a></li>
                 </ul>
             </nav>
+
+            <Drawer
+                id="mobile-navbar-drawer"
+                placement="right"
+                closable
+                closeIcon={<CloseIcon />}
+                visible={drawerVisible}
+                onClose={() => setDrawerVisible(false)}
+                style={{ padding: 0 }}
+            >
+                <ul className="links">
+                    <li onClick={() => setDrawerVisible(false)}><a href="#portfolio">Portfolio</a></li>
+                    <li onClick={() => setDrawerVisible(false)}><a href="#about">About</a></li>
+                    <li onClick={() => setDrawerVisible(false)}><a href="#contact">Contact</a></li>
+                </ul>
+            </Drawer>
         </header>
     );
 };
